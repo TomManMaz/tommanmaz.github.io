b32b964177ae6dc4e77936a33e1266af6ebec698
tommaso.mazzoli@tuwien.ac.at <tommaso.mazzoli@tuwien.ac.at>
2025-09-28 16:47:47 +0200

    improved visual things

--- html_converter.py
+++ html_converter.py
@@ -17,5 +17,5 @@ df_1['algorithm'] = df_1[['BP', 'SA', 'HC', 'TS', 'CMSA', 'LNS']].idxmin(axis=1)
 
 # only consider n_tours	n_legs, BKS and bound
 
-df_1 = df_1[['Instances', 'n_tours', 'n_legs', 'BKS', 'bound', 'gap', 'algorithm']]
+df_1 = df_1[['Instances', 'tours', 'legs', 'BKS', 'bound', 'gap', 'algorithm']]
 df_1.to_html('BKS_realistic_1.html', index=False)
